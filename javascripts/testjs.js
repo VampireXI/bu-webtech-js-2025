83f1a056898a8885795412a4ada6ba411c70c1c8
VampireXI <gapmanutsawin@gmail.com>
2025-10-03 12:59:48 +0700

    work js

--- javascripts/testjs.js
+++ javascripts/testjs.js
@@ -1,4 +1,4 @@
 let Num = Number(prompt("Enter your number => "))
 if (Num == 0){alert('Your number is => zero integer number')}
-if (Num %2 == 0 ){alert('Your number is => even integer number')}
+if (Num %2 == 0 && Num != 0){alert('Your number is => even integer number')}
 else {alert("Your number is => odd integer number")}
